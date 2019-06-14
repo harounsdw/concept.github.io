588fe72e94d7f4de59a2ffe37e2771198002957b
haroun <harounplanet@gmail.com>
2019-06-14 17:54:46 +0200

    add all

---
+++ search.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<div class="search-box">
+	
+	<input type="text" class="search-txt" name="" />
+	<a class="search-btn" href="#"><i class="fa fa-search"></i></a>
+</div>
+</body>
+</html>
